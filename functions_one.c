0868dbce1854b435d749b2767904103b8db1200b
21Kgold <claudiarpa@gmail.com>
2022-03-17 21:15:21 -0700

    binary print

--- functions_one.c
+++ functions_one.c
@@ -6,36 +6,40 @@
  * pcb - transform an unsigned base 10 number to a base 2 number
  * @argument: argument passed
  * Description: transform the an unsigned base 10 number n, to a base 2 number
- * and prints the b digits of the base 2 number.
+ * and prints the s[j] digits of the base 2 number.
  * Return: length of the base 2 number
  */
 int pcb(va_list argument)
 {
-	unsigned int n, i, j;
+	unsigned int n, m, i, j;
 	char *s;
 
 	n = va_arg(argument, int);
+	m = n;
 	if (n == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-	for (i = 0 ; n >= 1 ; i++)
+	for (i = 0 ; m >= 1 ; i++)
 	{
-		n = (n - n % 2) / 2;
+		m /= 2;
 	}
-	s = malloc( sizeof(char*) * (i + 1));
+	s = malloc((sizeof(char) * i) + 1);
 	if (s == NULL)
 	{
-		free(s);
 		return (-1);
 	}
-	for (j = i ; n >= 1 ; j--)
+	for (j = 1 ; n >= 1 ; j++)
 	{
 		s[j] = (n % 2) + '0';
-		n = (n - n % 2) / 2;
-		putchar(s[j]);
+		n /= 2;
+	}
+	s[j] = '\0';
+	for (i = j - 1 ; i > 0 ; i--)
+	{
+		_putchar(s[i]);
 	}
 	free(s);
-	return (i);
+	return (j);
 }
